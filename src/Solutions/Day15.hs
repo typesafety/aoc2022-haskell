32d5247865e1cdb18a879758647ed7376a0fb57d
typesafety <ailoodee@gmail.com>
2022-12-15 20:44:18 +0100

    Day 15: part 1, surely part 2 will be ez

--- src/Solutions/Day15.hs
+++ src/Solutions/Day15.hs
@@ -8,19 +8,22 @@ import Control.Lens.Combinators qualified as L
 import Control.Lens.Operators ((^.))
 import Control.Lens.TH qualified as LensTH
 import Control.Monad.State.Strict qualified as State
+import Data.Foldable (foldl')
 import Data.IntMap.Strict qualified as IM
+import Data.List qualified as List
 import Data.Sequence qualified as Seq
 
 import Text.Megaparsec qualified as P
 import Text.Megaparsec.Char qualified as P
 import Text.Megaparsec.Char.Lexer qualified as Lex
 
+import Debug.Trace
+
 
 
 -- $(LensTH.makeLenses ''???)
 
-
--- * Parsing
+-- * Data
 
 newtype Beacon = Beacon { _unBeacon :: (Int, Int) }
     deriving (Eq, Show)
@@ -32,6 +35,8 @@ data Sensor = Sensor
     }
     deriving (Eq, Show)
 $(LensTH.makeLenses ''Sensor)
+
+-- * Parsing
 
 type Parser = P.Parsec Void Text
 
@@ -60,11 +65,46 @@ rowP = do
 inputP :: Parser (NonEmpty Sensor)
 inputP = partialNonEmpty <$> P.someTill rowP P.eof
 
+-- * Logic
+
+manhattanS :: Sensor -> Int
+manhattanS s = manhattan (s ^. sLoc) (s ^. sBeacon ^. unBeacon)
+
+manhattan :: (Int, Int) -> (Int, Int) -> Int
+manhattan (x1, y1) (x2, y2) = abs (x1 - x2) + abs (y1 - y2)
+
+inRange :: (Int, Int) -> Sensor -> Bool
+inRange p s = manhattanS s >= manhattan p (s ^. sLoc) && p /= (s ^. sBeacon ^. unBeacon)
+
+cannotBe :: (Int, Int) -> NonEmpty Sensor -> Bool
+cannotBe p = any (inRange p)
+
+xbounds :: NonEmpty Sensor -> (Int, Int)
+xbounds ss =
+    let sloc0 = head ss ^. sLoc ^. L._1'
+    in foldl' f (sloc0, sloc0) ss
+  where
+    f :: (Int, Int) -> Sensor -> (Int, Int)
+    f (leftmost, rightmost) s =
+        (min (leftest s) leftmost, max (rightest s) rightmost)
+
+    leftest :: Sensor -> Int
+    leftest s = (s ^. sLoc ^. L._1') - manhattanS s 
+
+    rightest :: Sensor -> Int
+    rightest s = (s ^. sLoc ^. L._1') + manhattanS s
+
+solve1atY :: NonEmpty Sensor -> Int -> Int
+solve1atY ss y =
+    let (start, end) = xbounds ss
+        bs :: NonEmpty Bool = fmap (flip cannotBe ss) (zip [start .. end] (repeat y))
+     in List.length $ filter (\b -> b) bs
+
 -- * Solvers
 
 -- | Solve part 1.
 solve1 :: Text -> SolverResult
-solve1 = todo
+solve1 = SR . flip solve1atY 2_000_000 . partialParseText inputP
 
 -- | Solve part 2.
 solve2 :: Text -> SolverResult
