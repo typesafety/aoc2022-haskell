cfbdc3a3d37d4ca799574f1f57a23e5f19206248
typesafety <ailoodee@gmail.com>
2022-12-15 22:00:59 +0100

    Day 15: part 2

--- src/Solutions/Day15.hs
+++ src/Solutions/Day15.hs
@@ -7,21 +7,13 @@ import Prelube
 import Control.Lens.Combinators qualified as L
 import Control.Lens.Operators ((^.))
 import Control.Lens.TH qualified as LensTH
-import Control.Monad.State.Strict qualified as State
 import Data.Foldable (foldl')
-import Data.IntMap.Strict qualified as IM
 import Data.List qualified as List
-import Data.Sequence qualified as Seq
 
 import Text.Megaparsec qualified as P
 import Text.Megaparsec.Char qualified as P
 import Text.Megaparsec.Char.Lexer qualified as Lex
 
-import Debug.Trace
-
-
-
--- $(LensTH.makeLenses ''???)
 
 -- * Data
 
@@ -73,11 +65,13 @@ manhattanS s = manhattan (s ^. sLoc) (s ^. sBeacon ^. unBeacon)
 manhattan :: (Int, Int) -> (Int, Int) -> Int
 manhattan (x1, y1) (x2, y2) = abs (x1 - x2) + abs (y1 - y2)
 
-inRange :: (Int, Int) -> Sensor -> Bool
-inRange p s = manhattanS s >= manhattan p (s ^. sLoc) && p /= (s ^. sBeacon ^. unBeacon)
+inSensorRange :: (Int, Int) -> Sensor -> Bool
+inSensorRange p s =
+    manhattanS s >= manhattan p (s ^. sLoc)
+    && p /= (s ^. sBeacon ^. unBeacon)
 
 cannotBe :: (Int, Int) -> NonEmpty Sensor -> Bool
-cannotBe p = any (inRange p)
+cannotBe p = any (inSensorRange p)
 
 xbounds :: NonEmpty Sensor -> (Int, Int)
 xbounds ss =
@@ -98,7 +92,62 @@ solve1atY :: NonEmpty Sensor -> Int -> Int
 solve1atY ss y =
     let (start, end) = xbounds ss
         bs :: NonEmpty Bool = fmap (flip cannotBe ss) (zip [start .. end] (repeat y))
-     in List.length $ filter (\b -> b) bs
+     in List.length $ filter id bs
+
+-- * Part 2
+
+data Rhombus = Rhombus
+    { _rOrigo :: !(Int, Int)
+    , _rMh :: !Int  -- Manhattan
+    }
+    deriving (Eq, Show)
+$(LensTH.makeLenses ''Rhombus)
+
+sensorToRhombus :: Sensor -> Rhombus
+sensorToRhombus s = Rhombus
+    { _rOrigo = s ^. sLoc
+    , _rMh = manhattanS s
+    }
+
+xrangeAt :: Int -> Rhombus -> (Int, Int)
+xrangeAt y r =
+    let (rx, ry) = r ^. rOrigo
+        rmh = r ^. rMh
+        dy = abs (ry - y)
+        lb = rx - (rmh - dy)
+        rb = rx + (rmh - dy)
+     in (lb, rb)
+
+inRange :: Int -> (Int, Int) -> Bool
+inRange n (x, y) = n >= x && n <= y
+
+limit :: Int
+limit = 4_000_000
+
+searchRowY :: Int -> [Rhombus] -> Maybe (Int, Int)
+searchRowY y rhombi = go 0
+  where
+    go :: Int -> Maybe (Int, Int)
+    go x
+        | x >= limit = Nothing
+        | otherwise = case skip x rhombi of
+            Just newX -> go newX
+            Nothing -> Just (x, y)
+
+    skip :: Int -> [Rhombus] -> Maybe Int
+    skip _ [] = Nothing
+    skip n (r : rs)
+        | n `inRange` (lb, rb) = Just (rb + 1)
+        | otherwise = skip n rs
+      where
+        (lb, rb) = xrangeAt y r
+ 
+searchGrid :: [Rhombus] -> Maybe (Int, Int)
+searchGrid rs = go [0 .. limit]
+  where
+    go :: [Int] -> Maybe (Int, Int)
+    go [] = Nothing
+    go (y : ys) = searchRowY y rs <|> go ys
 
 -- * Solvers
 
@@ -108,4 +157,11 @@ solve1 = SR . flip solve1atY 2_000_000 . partialParseText inputP
 
 -- | Solve part 2.
 solve2 :: Text -> SolverResult
-solve2 = todo
+solve2 = SR
+    . uncurry (+)
+    . first (* limit)
+    . partialFromJust
+    . searchGrid
+    . fmap sensorToRhombus
+    . toList
+    . partialParseText inputP
